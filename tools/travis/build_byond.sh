71e280a53f7093d1f11bc3c825b832d82fb3d09f
Letter N <24603524+LetterN@users.noreply.github.com>
2019-05-17 13:58:44 +0800

    Update build_byond.sh

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -51,11 +51,11 @@ if [ "$BUILD_TOOLS" = false ]; then
 	else
 		tools/travis/dm.sh -DTRAVISBUILDING tgstation.dme
 		
-		sudo tools/deploy.sh travis_test
-		mkdir travis_test/config
+		# tools/deploy.sh travis_test someone fix this plz
+		#mkdir travis_test/config
 
 		#test config
-		cp tools/travis/travis_config.txt travis_test/config/config.txt
+		cp tools/travis/travis_config.txt config/config.txt #travis_test/
 
 		# get libmariadb, cache it so limmex doesn't get angery
 		if [ -f $HOME/libmariadb ]; then
@@ -70,11 +70,11 @@ if [ "$BUILD_TOOLS" = false ]; then
 			rm -rf mariadb_client-2.0.0-linux.tgz mariadb_client-2.0.0-linux
 		fi
 	
-		cd travis_test
+		#cd travis_test
     	ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
 		DreamDaemon tgstation.dmb -close -trusted -verbose -params "test-run&log-directory=travis"
-		cd ..
-		cat travis_test/data/logs/travis/clean_run.lk
+		#cd ..
+		cat data/logs/travis/clean_run.lk #travis_test/
 
 	fi;
 fi;
