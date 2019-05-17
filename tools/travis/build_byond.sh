221215674287d03fd8bb62b54413f8d02b99f6cf
Letter N <24603524+LetterN@users.noreply.github.com>
2019-05-17 14:22:02 +0800

    Update build_byond.sh

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -71,7 +71,7 @@ if [ "$BUILD_TOOLS" = false ]; then
 		fi
 	
 		#cd travis_test
-    	ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
+    	#ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
 		DreamDaemon tgstation.dmb -close -trusted -verbose -params "test-run&log-directory=travis"
 		#cd ..
 		cat data/logs/travis/clean_run.lk #travis_test/
